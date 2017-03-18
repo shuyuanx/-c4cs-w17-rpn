b30230d60cac2e502a2d21fafc29212e14b6222c
Shuyuan Xiao <shuyuanx@umich.edu>
2017-03-18 15:04:43 -0400

    Added implementation for ^

--- test_rpn.py
+++ test_rpn.py
@@ -11,6 +11,10 @@ class TestBasics(unittest.TestCase):
 	def test_exponentiation(self):
 		newResult = rpn.calculate('2 3 ^')
 		self.assertEqual(8, newResult)
+		newResult = rpn.calculate('2 5 ^')
+		self.assertEqual(32, newResult)
+		newResult = rpn.calculate('3 2 ^')
+		self.assertEqual(9, newResult)
 
 
 if __name__ == "__main__":
